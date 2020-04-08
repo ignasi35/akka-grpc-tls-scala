ef29859995b09d46eddc7b8e594c7564299fd55d
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-04-08 19:33:54 +0200

    Soem cleanup and formatting

--- src/main/scala/com/example/helloworld/GreeterServer.scala
+++ src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -1,11 +1,9 @@
 package com.example.helloworld
 
-//#import
-
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import akka.actor.ActorSystem
-import akka.http.scaladsl.{ Http, HttpConnectionContext }
+import akka.http.scaladsl.{Http, HttpConnectionContext}
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.UseHttp2.Always
@@ -13,19 +11,17 @@ import akka.stream.ActorMaterializer
 import akka.stream.Materializer
 import com.typesafe.config.ConfigFactory
 
-//#import
-
-
-//#server
 object GreeterServer {
 
   def main(args: Array[String]): Unit = {
     // important to enable HTTP/2 in ActorSystem's config
-    val conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
+    val conf = ConfigFactory
+      .parseString("akka.http.server.preview.enable-http2 = on")
       .withFallback(ConfigFactory.defaultApplication())
     val system: ActorSystem = ActorSystem("HelloWorld", conf)
     new GreeterServer(system).run()
   }
+
 }
 
 class GreeterServer(system: ActorSystem) {
@@ -52,4 +48,3 @@ class GreeterServer(system: ActorSystem) {
     bound
   }
 }
-//#server
